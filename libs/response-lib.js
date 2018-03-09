420aa7ea95d681ba64ac51daff5a6116a5d5e31e
kyle <kdeak1210@gmail.com>
2018-03-08 19:08:03 -0500

    Refactored code to use lib functions for dynamoDb calls & server responses (error and success)
    The dynamo-lib 'call' function will returns a promise, so that it can be used with ES7 async/await for more concise, readable code

---
+++ libs/response-lib.js
@@ -0,0 +1,21 @@
+/* Manages building response objects for both success and failure cases,
+using the proper HTTP status code and headers */
+
+export function success(body) {
+  return buildResponse(200, body);
+}
+
+export function failure(body) {
+  return buildResponse(500, body);
+}
+
+function buildResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify(body)
+  };
+}
